338e26202a5321909df62823fc3fd058c3086007
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:03:49 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -16,7 +16,7 @@ output "mssql_failover_groups_partner_server" {
 }
 output "mssql_failover_groups_read_write_endpoint_failover_policy" {
   description = "Map of read_write_endpoint_failover_policy values across all mssql_failover_groups, keyed the same as var.mssql_failover_groups"
-  value       = { for k, v in azurerm_mssql_failover_group.mssql_failover_groups : k => v.read_write_endpoint_failover_policy if v.read_write_endpoint_failover_policy != null && length(v.read_write_endpoint_failover_policy) > 0 }
+  value       = { for k, v in azurerm_mssql_failover_group.mssql_failover_groups : k => one(v.read_write_endpoint_failover_policy) if v.read_write_endpoint_failover_policy != null && length(v.read_write_endpoint_failover_policy) > 0 }
 }
 output "mssql_failover_groups_readonly_endpoint_failover_policy_enabled" {
   description = "Map of readonly_endpoint_failover_policy_enabled values across all mssql_failover_groups, keyed the same as var.mssql_failover_groups"
